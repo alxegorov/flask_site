3e5e91318cc54a2596bef350f72baa840dab6373
alxegorov <alexey.ig.egorov@gmail.com>
2019-07-15 10:50:46 +0300

    Config for database

--- config.py
+++ config.py
@@ -1,3 +1,22 @@
+import os
+
+
 # Protection
+from typing import Union
+
 CSRF_ENABLED = True
 SECRET_KEY = "Kids aren't alright"
+
+# OpenID
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
+# Database
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO: Union[bytes, str] = os.path.join(basedir, 'db_repository')
