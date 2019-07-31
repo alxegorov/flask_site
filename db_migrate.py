1729f372104a66a28a0ac80a2bcf7bd9a7504e07
alexey.i.egorov <alexey.i.egorov@megafon.ru>
2019-08-01 00:09:42 +0300

    db migration

--- db_migrate.py
+++ db_migrate.py
@@ -14,5 +14,6 @@ script = api.make_update_script_for_model(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MI
                                           db.metadata)
 open(migration, 'wt').write(script)
 api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 print('New migration saved as' + migration)
 print("Current database version:" + str(v))
