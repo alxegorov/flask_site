66d58a3d6a1ca711ac0df6add919a4c4a78e26ec
alexey.i.egorov <alexey.i.egorov@megafon.ru>
2019-08-01 17:10:54 +0300

    Login form

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
-from flask import render_template, flash, redirect
-from app import app
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask_login import login_user, logout_user, current_user, login_required
+from app import app, db, lm, oid
+from .forms import LoginForm
+from .models import User
 
 
 @app.route('/')
@@ -23,13 +25,36 @@ def index():
                            posts=posts)
 
 
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
 @app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
-        return redirect('/index')
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
     return render_template('login.html',
                            title='Sign In',
                            form=form,
                            providers=app.config['OPENID_PROVIDERS'])
+
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash('Invalid login. Please try again.')
+        return redirect(url_for('login'))
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        user = User(nickname=nickname, email=resp.email)
+        db.session.add(user)
+        db.session.commit()
