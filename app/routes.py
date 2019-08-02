6271cfcccb8763b4dbe025b9ab5364a26bbc4431
alexey.i.egorov <alexey.i.egorov@megafon.ru>
2019-08-03 00:58:23 +0300

    Login form

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,6 @@
-from flask import render_template, flash, redirect, session, url_for, request, g
-from flask_login import login_user, logout_user, current_user, login_required
+from flask import render_template
 from app import app
-from .forms import LoginForm
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -18,7 +17,10 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html',
-                           title='Home',
-                           user=user,
-                           posts=posts)
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
