cc16cccf0a80c19ff7077020794b9c4ff4a6e645
alexey.i.egorov <alexey.i.egorov@megafon.ru>
2019-08-15 09:17:36 +0300

    Translate

--- app/translate.py
+++ app/translate.py
@@ -1,1 +1,15 @@
-c
+import json
+import requests
+from flask_babel import _
+from app import app
+
+
+def translate(text, source_language, dest_language):
+    if 'MS_TRANSLATOR_KEY' not in app.config or not app.config['MS_TRANSLATOR_KEY']:
+        return _('Error: the translation service is not configured.')
+    lang = source_language + "-" + dest_language
+    r = requests.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key={}&text={}&lang={}'.format(
+        app.config['MS_TRANSLATOR_KEY'], text, lang))
+    if r.status_code != 200:
+        return _('Error: the translation service failed.')
+    return json.loads(r.content.decode('utf-8-sig'))['text'][0]
