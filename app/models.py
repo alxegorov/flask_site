cf567beb2f1bbee5c81aeb738ec163a7651386ac
alexey.i.egorov <alexey.i.egorov@megafon.ru>
2019-08-17 00:46:43 +0300

    Blueprint

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
 from datetime import datetime
-from app import app, db, login
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
 from hashlib import md5
-import jwt
 from time import time
+from flask import current_app
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+from app import db, login
 
 followers = db.Table('followers',
                      db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
@@ -54,7 +55,7 @@ class User(UserMixin, db.Model):
         return followed.union(own).order_by(Post.timestamp.desc())
 
     def get_reset_password_token(self, expires_in=600):
-        return jwt.encode({'reset_password': self.id, 'exp': time() + expires_in}, app.config['SECRET_KEY'],
+        return jwt.encode({'reset_password': self.id, 'exp': time() + expires_in}, current_app.config['SECRET_KEY'],
                           algorithm='HS256').decode('utf-8')
 
     @staticmethod
--- app/translate.py
+++ app/translate.py
@@ -1,15 +1,15 @@
 import json
 import requests
 from flask_babel import _
-from app import app
+from flask import current_app
 
 
 def translate(text, source_language, dest_language):
-    if 'MS_TRANSLATOR_KEY' not in app.config or not app.config['MS_TRANSLATOR_KEY']:
+    if 'MS_TRANSLATOR_KEY' not in current_app.config or not current_app.config['MS_TRANSLATOR_KEY']:
         return _('Error: the translation service is not configured.')
     lang = source_language + "-" + dest_language
     r = requests.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key={}&text={}&lang={}'.format(
-        app.config['MS_TRANSLATOR_KEY'], text, lang))
+        current_app.config['MS_TRANSLATOR_KEY'], text, lang))
     if r.status_code != 200:
         return _('Error: the translation service failed.')
     return json.loads(r.content.decode('utf-8-sig'))['text'][0]
